01936f8647bf07a99baa87561edbaba27536d92a
superpasses <165167006+superpasses@users.noreply.github.com>
2024-06-25 22:27:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://okie-inqt.onrender.com"
+        URL = "https://okie-rq9p.onrender.com"
     else:
-        URL = "https://okie-inqt.onrender.com"
+        URL = "https://okie-rq9p.onrender.com"
 
 
 
